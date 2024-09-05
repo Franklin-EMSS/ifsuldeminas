5f63d54fd311f09ed47928d6434adfd7e6684d71
franklin <frank.oprogramador@gmail.com>
2024-09-04 22:43:35 -0300

    second commit

--- database/migrations/2024_09_01_222803_create_produtos_table.php
+++ database/migrations/2024_09_01_222803_create_produtos_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->integer("qtd");
             $table->float("precoUntario");
             $table->float("precoVenda");
+            $table->timestamps();
         });
     }
 
